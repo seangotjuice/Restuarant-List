254c877eccada88ae60bf561983632006c08f153
Sean Chen <seanchen112@gmail.com>
2023-05-25 14:04:22 +0800

    feat: add user registration feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../../models/user");
 
 router.get("/login", (req, res) => {
   res.render("login");
@@ -7,6 +8,25 @@ router.get("/login", (req, res) => {
 
 router.get("/register", (req, res) => {
   res.render("register");
+});
+router.post("/register", (req, res) => {
+  const { name, email, password, confirmpassword } = req.body;
+  User.findOne({ email }).then((user) => {
+    if (user) {
+      console.log("already registered");
+      // 停在原頁面，但保留使用者輸入的表單內容
+      res.render("register", {
+        name,
+        email,
+        password,
+        confirmpassword,
+      });
+    } else {
+      return User.create({ name: name || "No Name", email, password })
+        .then(() => res.redirect("/"))
+        .catch((err) => console.log(err));
+    }
+  });
 });
 
 module.exports = router;
