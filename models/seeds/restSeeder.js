d53ae55cde104131db287554ee221afd11ffd3cb
Sean Chen <seanchen112@gmail.com>
2023-05-29 13:59:21 +0800

    modify todoSeeder

--- models/seeds/restSeeder.js
+++ models/seeds/restSeeder.js
@@ -2,31 +2,26 @@ const Rest = require("../rest");
 const mongoose = require("mongoose");
 const restList = require("../../restaurant.json").results;
 const db = require("../../config/mongoose");
-
-// if (process.env.NODE_ENV !== "production") {
-//   require("dotenv").config();
-// }
-// mongoose.connect(process.env.MONGODB_URI); // 設定連線到 mongoDB
-// mongoose.connect(process.env.MONGODB_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-// // 取得資料庫連線狀態
-// const db = mongoose.connection;
-// // 連線異常
-// db.on("error", () => {
-//   console.log("mongodb error!");
-// });
-// 連線成功
+const bcrypt = require("bcryptjs");
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
 db.once("open", () => {
-  console.log("running restSeeder.js...");
-  Rest.create(restList)
-    .then(() => {
-      console.log("restSeeder done!");
-      db.close();
+  bcrypt
+    .genSalt(10)
+    .then((salt) => bcrypt.hash("12345", salt))
+    .then((hash) => {
+      Rest.create({
+        name: "root",
+        email: "root@root.root",
+        password: hash,
+      });
     })
-    .catch((err) => console.log(err));
+    .then(() => {
+      console.log("done");
+      process.exit();
+    });
 });
 
 module.exports = db;
