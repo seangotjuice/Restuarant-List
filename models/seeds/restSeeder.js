cce35c647863a3f64692f250f8af492024811da7
Sean Chen <seanchen112@gmail.com>
2023-07-14 10:45:47 +0800

    refactor seeder

--- models/seeds/restSeeder.js
+++ models/seeds/restSeeder.js
@@ -1,4 +1,5 @@
 const Rest = require("../rest");
+const User = require("../user");
 const mongoose = require("mongoose");
 const restList = require("../../restaurant.json").results;
 const db = require("../../config/mongoose");
@@ -7,16 +8,45 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
+const SEED_USER = {
+  name: "root",
+  email: "root@example.com",
+  password: "12345678",
+};
+
+// 以下重寫 ：create user, create 10 筆餐廳種子資料 用promise.all
+
 db.once("open", () => {
   bcrypt
     .genSalt(10)
-    .then((salt) => bcrypt.hash("12345", salt))
-    .then((hash) => {
-      Rest.create({
-        name: "root",
-        email: "root@root.root",
+    .then((salt) => bcrypt.hash(SEED_USER.password, salt))
+    .then((hash) =>
+      User.create({
+        name: SEED_USER.name,
+        email: SEED_USER.email,
         password: hash,
-      });
+      })
+    )
+    .then((user) => {
+      const userId = user._id;
+      return Promise.all(
+        Array.from({ length: 10 }, (_, i) =>
+          Rest.create({
+            name: `name-${i}`,
+            description: "des",
+            rating: 1,
+            google_map: "www.google.com",
+            phone: "0800",
+            location: "www.google.com",
+            image: `https://loremflickr.com/320/240/restaurant,food/?random=${
+              Math.random() * 100
+            }`,
+            category: "category",
+            name_en: "en",
+            userId,
+          })
+        )
+      );
     })
     .then(() => {
       console.log("done");
@@ -24,4 +54,21 @@ db.once("open", () => {
     });
 });
 
-module.exports = db;
+// db.once("open", () => {
+//   bcrypt
+//     .genSalt(10)
+//     .then((salt) => bcrypt.hash("12345", salt))
+//     .then((hash) => {
+//       Rest.create({
+//         name: "root",
+//         email: "root@root.root",
+//         password: hash,
+//       });
+//     })
+//     .then(() => {
+//       console.log("done");
+//       process.exit();
+//     });
+// });
+
+// module.exports = db;
